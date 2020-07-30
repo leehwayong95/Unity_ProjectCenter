784a6f170c93ea5af7bbc8745532050b5c5e6b2c
이화용 <dlghkdyd500@gmail.com>
2020-07-30 11:08:03 +0900

    add movement
    add employee movement
    add show employee name

--- Project.Center/Assets/Script/GameManager.cs
+++ Project.Center/Assets/Script/GameManager.cs
@@ -110,5 +110,24 @@ public class GameManager : MonoBehaviour
         ProjectName.text = StageName[stage];
     }
 
+    public static string enemy_name
+    {
+        get
+        {
+            string[] names = new string[8];
+            names[0] = "화용";
+            names[1] = "정열";
+            names[2] = "인영";
+            names[3] = "용훈";
+            names[4] = "동원";
+            names[5] = "유정";
+            names[6] = "덕용";
+            names[7] = "형미";
+
+            return names[Random.Range(0, names.Length)];
+        }
+    }
+
+
     //IEnumerator showPanel_Gauge()
 }
