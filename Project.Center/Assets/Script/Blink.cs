c96d7adfbd820994648d3aa752f483b9831e5b01
jay9884 <68951034+jay9884@users.noreply.github.com>
2020-07-30 09:56:58 +0900

    Version1.0
    Version정열

---
+++ Project.Center/Assets/Script/Blink.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Blink : MonoBehaviour
+{
+    Text blink_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blink_Text = GetComponent<Text>();
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while(true)
+        {
+            blink_Text.text = "";
+            yield return new WaitForSeconds(.5f);
+            blink_Text.text = "Press to start";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project.Center/Assets/Script/CoffeeDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeDrop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Drop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Drop()
+    {
+        float t = 0.3f;
+
+        while (t > 0)
+        {
+            transform.Translate(Vector3.down * 0.05f);
+            t -= Time.deltaTime;
+            yield return null;
+        }
+    }
+}
